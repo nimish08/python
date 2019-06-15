798f54f0322c5b29b199c32c017dfa21fc7554fc
nimish08 <mittalnimish8@gmail.com>
2019-06-15 19:08:23 +0000

    Update cruser.py

--- cruser.py
+++ cruser.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-
 import os
-import  
-'''
-string=input("enter a string")
-print(string.isalpha())
- '''
 
-useradd=input("selsect Username")
-passwd=input("Select Password")
-os.system("sudo useradd -m -p "+passwd+" "+useradd)
+uname=input("enter input from user")
+a=uname.isalpha()
+
+if a ==True:
+        passwd="hello"+uname
+        os.system("sudo useradd -m -p "+passwd+" "+uname)
+        print("user created!")
+else:
+        print("all charcter are not in string!")
 
 
 
